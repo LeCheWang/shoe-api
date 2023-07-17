c0852cef188374ffae6770e0653138fbe9ee77e2
LeCheWang <haphonggoroki@gmail.com>
2023-07-17 11:06:38 +0700

    tesst cicd

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,13 @@
 const accountRouter = require('./account.router');
-const shoeRouter = require("./shoe.router")
+const shoeRouter = require('./shoe.router');
 
 module.exports = (app) => {
   app.use('/api/accounts', accountRouter);
   app.use('/api/shoes', shoeRouter);
+  app.use('/*', (req, res) => {
+    res.json({
+      statusCode: 404,
+      message: 'not found',
+    });
+  });
 };
