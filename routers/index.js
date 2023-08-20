610127251310557ad852b11b5ff33ff8f8d66439
LeCheWang <haphonggoroki@gmail.com>
2023-08-20 18:45:39 +0700

    fix cart

--- routers/index.js
+++ routers/index.js
@@ -5,7 +5,7 @@ const cardRouter = require('./card.router');
 module.exports = (app) => {
   app.use('/api/accounts', accountRouter);
   app.use('/api/shoes', shoeRouter);
-  app.use('/api/cards', cardRouter);
+  app.use('/api/carts', cardRouter);
   app.use('/*', (req, res) => {
     res.json({
       statusCode: 404,
