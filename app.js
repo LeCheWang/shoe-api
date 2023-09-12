77bb9af1c6ff912eabbfc26c2e2f720e4b7c06d3
LeCheWang <haphonggoroki@gmail.com>
2023-09-12 11:36:27 +0700

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require("cors");
 
 const connectDB = require('./configs/database');
 const routers = require('./routers');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
